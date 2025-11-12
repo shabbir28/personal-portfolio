2ce6e1c35855e3db5d5a5fb5bd445f2ec35fc6d1
Shabbir Ahmed <shabbirfa21099@gmail.com>
2025-11-12 18:50:24 +0500

    name updation in hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
       <div className="container max-w-4xl mx-auto text-center z-10">
         <div className="space-y-6">
           <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
-            <span className="opacity-0 animate-fade-in">Hi, I'm</span>
+            <span className="opacity-0 animate-fade-in">Hi, I'm Muhammad</span>
             <span className="text-primary opacity-0 animate-fade-in-delay-1">
               {" "}Shabbir
             </span>
